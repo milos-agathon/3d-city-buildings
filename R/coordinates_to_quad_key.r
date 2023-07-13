a24fa891eec890d6df71dfde593bfc34d037c59f
Milos Popovic <37574511+milos-agathon@users.noreply.github.com>
2023-07-13 22:04:35 +0200

    Update coordinates_to_quad_key.r

--- R/coordinates_to_quad_key.r
+++ R/coordinates_to_quad_key.r
@@ -1,1 +1,34 @@
+# function to get quad key
+tileXYToQuadKey <- function(tileX, tileY, zoom) {
+    quadKey <- ""
+    for (i in seq(zoom, 1)) {
+        digit <- 0
+        mask <- bitwShiftL(1, i - 1)
+        if (bitwAnd(tileX, mask) != 0) {
+            digit <- digit + 1
+        }
+        if (bitwAnd(tileY, mask) != 0) {
+            digit <- digit + 2
+        }
+        quadKey <- paste0(
+            quadKey, as.character(
+                digit
+            )
+        )
+    }
+    return(quadKey)
+}
 
+latLongToTileXY <- function(latitude, longitude, zoom) {
+    sinLatitude <- sin(latitude * pi / 180)
+    pixelX <- (
+        (longitude + 180) / 360) * 256 * (2^zoom)
+    pixelY <- (
+        0.5 - log(
+            (1 + sinLatitude) / (1 - sinLatitude)
+        ) / (4 * pi)
+    ) * 256 * (2^zoom)
+    tileX <- floor(pixelX / 256)
+    tileY <- floor(pixelY / 256)
+    return(list(tileX = tileX, tileY = tileY))
+}
